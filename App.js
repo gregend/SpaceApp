7108925854e2f310cbf5d2793fce3f67e36a44c6
gregend <drizzt52@gmail.com>
2017-09-24 15:16:36 +0200

    stucture cleanup, onLongPress

--- App.js
+++ App.js
@@ -3,32 +3,30 @@ import {
   TouchableWithoutFeedback, TouchableOpacity, Text,
   View, Image, StatusBar, ScrollView, Picker, TextInput, Button
 } from 'react-native';
+import DataProvider from './lib/dataProviders/dataProvider.js'
 import Dimensions from 'Dimensions'
 import { styles } from './styles/main.js'
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     const d = new Date();
-    console.log(Dimensions.get('window'))
-    this.year = d.getUTCFullYear();
-    this.month = d.getMonth() + 1;
-    this.day = d.getDate();
     this.state = {
       isLoading: true,
       dateYear: d.getUTCFullYear(),
       dateMonth: d.getMonth() + 1,
       dateDay: d.getDate(),
-      height: 100,
-      dim: Dimensions
+      dim: Dimensions,
+      getData: new DataProvider().getData,
+
     }
   }
 
   dateStuff() {
-    const newDay = this.state.dateDay - 1;
+    const newDay = Number(this.state.dateDay) - 1;
     if (newDay <= 0) {
       this.setState({
         dateDay: 30,
-        dateMonth: this.state.dateMonth - 1,
+        dateMonth: Number(this.state.dateMonth) - 1,
       })
     } else {
       this.setState({
@@ -38,20 +36,6 @@ export default class App extends React.Component {
   }
   componentDidMount() {
     this.fetchFromApi();
-  }
-
-  handleImagePress() {
-    if (this.state.height === 100) {
-      this.setState({
-        height: 500
-      })
-    }
-    else {
-      this.setState({
-        height: 100
-      })
-    }
-
   }
   componentWillUpdate() {
   }
@@ -66,9 +50,7 @@ export default class App extends React.Component {
       isLoading: true
     });
     const state = this.state;
-    const dateString = state.dateYear + '-' + state.dateMonth + '-' + state.dateDay;
-    return fetch('https://api.nasa.gov/planetary/apod?api_key=VOJbRrExRvLUhD8sr90T4FpF4VcYRRIF1G7AhSEu&date=' + dateString)
-      .then(data => data.json())
+    this.state.getData(state)
       .then(data => {
         // this.dateStuff();
         this.setState({
@@ -87,6 +69,9 @@ export default class App extends React.Component {
   onLayout = () => {
     const { width, height } = this.state.dim.get('window')
     const orientation = width > height ? 'horizontal' : 'vertical';
+    if (orientation === 'horizontal') {
+
+    }
     this.setState({
       width: width,
       height: height / 2,
@@ -102,23 +87,27 @@ export default class App extends React.Component {
 
           <View style={{ flex: 1, flexDirection: 'row' }}>
             <StatusBar hidden={true} />
-            <TextInput defaultValue={this.state.dateYear.toString()} onChangeText={(value) => { this.setState({dateYear: value})}} underlineColorAndroid='transparent' placeholder='YEAR' style={styles.datePickerYear} keyboardType='numeric' maxLength={4} />
-            <TextInput defaultValue={this.state.dateMonth.toString()} onChangeText={(value) => { this.setState({dateMonth: value}) }} underlineColorAndroid='transparent' placeholder='MON' style={styles.datePickerMonth} keyboardType='numeric' maxLength={2} />
-            <TextInput defaultValue={this.state.dateDay.toString()} onChangeText={(value) => {  this.setState({dateDay: value}) }} underlineColorAndroid='transparent'placeholder='DAY' style={styles.datePickerDay} keyboardType='numeric' maxLength={2} />
+            <TextInput value={this.state.dateYear.toString()} onChangeText={(value) => { this.setState({ dateYear: value }) }} underlineColorAndroid='transparent' placeholder='YEAR' style={styles.datePickerYear} keyboardType='numeric' maxLength={4} />
+            <TextInput value={this.state.dateMonth.toString()} onChangeText={(value) => { this.setState({ dateMonth: value }) }} underlineColorAndroid='transparent' placeholder='MON' style={styles.datePickerMonth} keyboardType='numeric' maxLength={2} />
+            <TextInput value={this.state.dateDay.toString()} onChangeText={(value) => { this.setState({ dateDay: value }) }} underlineColorAndroid='transparent' placeholder='DAY' style={styles.datePickerDay} keyboardType='numeric' maxLength={2} />
             <TouchableOpacity onPress={() => this.fetchFromApi()} style={styles.button}>
               <Text style={styles.dateButtonText}>GET</Text>
             </TouchableOpacity>
           </View>
 
 
-          <TouchableWithoutFeedback onLongPress={() => console.log('clicked')}>
+          <TouchableWithoutFeedback onLongPress={() => {
+            console.log('clicked')
+            this.dateStuff();
+            this.fetchFromApi();
+          }}>
             <Image resizeMode={'contain'} style={this.imageStyles()} source={{ uri: this.state.apod.url }} />
           </TouchableWithoutFeedback>
 
           <View style={styles.description}>
             <Text style={styles.descriptionText}>{this.state.apod.explanation}</Text>
           </View>
-          
+
         </ScrollView>
       );
     } else {
